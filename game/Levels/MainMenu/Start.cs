1b693432d9dd2c62b738f1727cfac46c1fae4703
pgh1686 <pgh1686>
2025-02-10 13:38:20 -0500

    Implemented basic main menu (Start and Quit)

---
+++ game/Levels/MainMenu/Start.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class Start : Button{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready(){
+		this.Pressed += Clicked;
+	}
+
+    private void Clicked(){
+		GetTree().ChangeSceneToFile("res://Main.tscn");
+    }
+}
