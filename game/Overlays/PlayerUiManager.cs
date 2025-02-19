8517266736f503eab02e668eb5a0a38dbc96089d
Colin Zopf <caz1158@rit.edu>
2025-02-19 13:24:42 -0500

    dynamic ammo and healthbar UI & manager setup.

---
+++ game/Overlays/PlayerUiManager.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+
+public partial class PlayerUiManager : Control
+{
+    ProgressBar healthbar;
+    TextureProgressBar ammobar;
+
+    public override void _Ready()
+    {
+        healthbar = (ProgressBar)GetNode("/root/Main/PlayerUi/HealthBar");
+        ammobar = (TextureProgressBar)GetNode("/root/Main/PlayerUi/AmmoCount");
+        healthbar.Value = healthbar.MaxValue;
+        ammobar.Value = ammobar.MaxValue;
+    }
+
+    public void IncrimentHealth(int value)
+    {
+        healthbar.Value += value;
+    }
+
+    public void DecrimentHealth(int value)
+    {
+        healthbar.Value -= value;
+    }
+
+    public void IncrimentMaxHealth(int value)
+    {
+        healthbar.MaxValue += value;
+        healthbar.Value = healthbar.MaxValue;
+    }
+
+    public void DecrimentMaxHealth(int value)
+    {
+        healthbar.MaxValue -= value;
+        healthbar.Value = healthbar.MaxValue;
+    }
+
+    public void IncrementAmmo(int value)
+    {
+        ammobar.Value += value;
+    }
+
+    public void DecrementAmmo(int value)
+    {
+        ammobar.Value -= value;
+    }
+
+    public void IncrementMaxAmmo(int value)
+    {
+        ammobar.MaxValue += value;
+        ammobar.Value = ammobar.MaxValue;
+    }
+
+    public void DecrementMaxAmmo(int value)
+    {
+        ammobar.MaxValue -= value;
+        ammobar.Value = ammobar.MaxValue;
+    }
+}
