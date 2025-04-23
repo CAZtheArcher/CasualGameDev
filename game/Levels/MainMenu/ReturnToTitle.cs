0031def3541b1a6b699882e98fdc901b1ac40d64
Colin Zopf <caz1158@rit.edu>
2025-04-23 13:54:10 -0400

    progress on tutorial screen

---
+++ game/Levels/MainMenu/ReturnToTitle.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class ReturnToTitle : Button
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        this.Pressed += Clicked;
+    }
+
+    private void Clicked()
+    {
+        GetTree().ChangeSceneToFile("res://Levels/MainMenu/MainMenu.tscn");
+    }
+}
